3ad2c1123ae5d18f0e7d8117195b2811ffee6631
Louisa Köhnen <louisa@koehnen.biz>
2024-12-01 19:09:40 +0100

    Switch to real input data

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::fs;
 use crate::calculation::calculate_distance;
 
 fn main() {
-    let file_path = "data/test.data";
+    let file_path = "data/input.data";
 
     let contents = fs::read_to_string(file_path)
         .expect("Should have been able to read the file");
