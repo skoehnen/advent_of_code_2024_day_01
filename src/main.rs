416314763c5cf759e4a65e0c23fdf1bce02400f4
Louisa Köhnen <louisa@koehnen.biz>
2024-12-01 15:05:13 +0100

    Add test for get

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+mod downloader;
+
 fn main() {
     println!("Hello, world!");
+    //let body = reqwest::blocking::get("https://www.rust-lang.org")?
+    //    .text()?;
+
+    let body = match reqwest::blocking::get("https://www.rust-lang.org") {
+        Ok(body) => body,
+        Err(error) => panic!("There was a problem with the get: {:?}", error),
+    };
+
+    let body_text = match body.text() {
+        Ok(body) => body,
+        Err(error) => panic!("There was a problem with the request: {:?}", error),
+    };
+
+    println!("body_text = {body_text:?}");
 }
