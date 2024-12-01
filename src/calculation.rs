20f8f403a9f3fd5cf0c8b2a5c3a39b62540d8a7a
Louisa Köhnen <louisa@koehnen.biz>
2024-12-01 18:06:22 +0100

    Add stub to sort vectors

--- src/calculation.rs
+++ src/calculation.rs
@@ -1,3 +1,10 @@
 pub fn calculate_distance(data: String) -> i64 {
     return 0;
 }
+
+fn sort_vectors(data: (Vec<i32>, Vec<i32>)) -> (Vec<i32>, Vec<i32>) {
+    let vec1 = Vec::new();
+    let vec2 = Vec::new();
+
+    return (vec1, vec2);
+}
