99d74c239b81b2492201e3dc8c3faa547e0e3168
Louisa Köhnen <louisa@koehnen.biz>
2024-12-01 17:50:14 +0100

    Generate vectors from input file

---
+++ src/data_transform.rs
@@ -0,0 +1,23 @@
+use std::str::Split;
+
+pub fn transform(data: String) -> (Vec<i32>, Vec<i32>){
+    let mut vec_left = Vec::new();
+    let mut vec_right = Vec::new();
+
+    for line in data.lines() {
+        let mut split_data = line.split("   ");
+
+        let left_str = split_data.next();
+        let right_str = split_data.next();
+
+        let left_number: i32 = left_str.unwrap().parse().unwrap();
+        let right_number: i32 = right_str.unwrap().parse().unwrap();
+
+        vec_left.push(left_number);
+        vec_right.push(right_number);
+    }
+
+    let result_tuple = (vec_left, vec_right);
+
+    return result_tuple;
+}
--- src/main.rs
+++ src/main.rs
@@ -10,4 +10,17 @@ fn main() {
         .expect("Should have been able to read the file");
 
     println!("With text:\n{contents}");
+    let split_data = data_transform::transform(contents);
+
+    let (vec_left, vec_right) = split_data;
+
+    for item in vec_left.iter() {
+        println!("{}", item)
+    }
+
+    println!(" ");
+
+    for item in vec_right.iter() {
+        println!("{}", item)
+    }
 }
